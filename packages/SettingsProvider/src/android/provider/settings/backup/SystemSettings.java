AOSPA/android_frameworks_base-old

base: Add custom vibration patterns [1/3] [SQUASH]
idoybh edits: Add settings value to backup array

Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
Co-authored-by: Vishalcj17 <vishalcj@aospa.co>

Squashed with:

  base: Allow customizing default notification vibration pattern [1/2]

    Re-use the patterns we've got for ringtone vibration as options
    In addition allow crafting a custom pattern same as we do for ringtone

    Change-Id: Id22be7675a5c0746a8d4fa0ed543fd80ff39f3fd

  base: Add different custom notification vibration pattern [1/2]

    * the ones used by calls don't really fit here

    Change-Id: Ib89d5421dc4055176f0ae15186da98b359d8f7c1

  base: Add different custom notification vibration pattern [1/2] (2)

    Change-Id: I3a82b5275c48f322c0b96f22a14bccada2d9c2df

Change-Id: I98d870f917f0470c14390d35c3ed08de2f8232a6

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -87,5 +87,7 @@ public class SystemSettings {
         Settings.System.DISPLAY_COLOR_MODE,
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
+        Settings.System.RINGTONE_VIBRATION_PATTERN,
+        Settings.System.NOTIFICATION_VIBRATION_PATTERN,
     };
 }

Mrick343/frameworks_base

packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -114,7 +114,9 @@ public class SystemSettings {
                 Settings.System.SCREEN_FLASH_NOTIFICATION_COLOR,
                 Settings.System.NOTIFICATION_COOLDOWN_ENABLED,
                 Settings.System.NOTIFICATION_COOLDOWN_ALL,
-                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED
+                Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
+                Settings.System.RINGTONE_VIBRATION_PATTERN,
+                Settings.System.NOTIFICATION_VIBRATION_PATTERN
         ));
         if (Flags.backUpSmoothDisplayAndForcePeakRefreshRate()) {
             settings.add(Settings.System.PEAK_REFRESH_RATE);